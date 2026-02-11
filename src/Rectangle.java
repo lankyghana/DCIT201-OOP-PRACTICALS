4dc75b92c17e78b28f54bbd598ea7eb2fb0939e2
lankyghana <dktakyi001@st.ug.edu.gh>
2026-02-11 01:22:32 +0000

    Rectangle Immutability

---
+++ src/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+    private double length;
+    private double width;
+
+    //Constructors
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+    public double getArea() {
+        return width * length;
+    }
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+}
