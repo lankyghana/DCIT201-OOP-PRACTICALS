64f8e4dec92ea94dc61659c0646bea0a0ea604ef
lankyghana <dktakyi001@st.ug.edu.gh>
2026-02-11 01:18:00 +0000

    main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(7.0, 5.0);
+        System.out.println("The Area is: " + r1.getArea());
+        System.out.println("The Parameter is: " + r1.getPerimeter());
+    }
+}
