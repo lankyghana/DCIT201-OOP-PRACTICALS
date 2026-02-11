9675686ae32484c6c6f1b27a45295f65c079f0ae
lankyghana <dktakyi001@st.ug.edu.gh>
2026-02-11 01:29:21 +0000

    Password Protected User

---
+++ src/User.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class User {
+    private String Username;
+    private String Password;
+
+    public User(String username, String password) {
+        this.Username = username;
+        this.Password = password;
+    }
+}
